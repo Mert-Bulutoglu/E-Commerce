dd4cb7fe42baa3545a26dc02b2c2c5c292cffa5c
Mert Bulutoglu <mertbulutoglu95@gmail.com>
2022-12-01 20:50:14 +0300

    Last Updates

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -22,11 +22,11 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products =  await _context.Products.ToListAsync();
+            var products = await _context.Products.ToListAsync();
 
             return Ok(products);
         }
-        
+
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
