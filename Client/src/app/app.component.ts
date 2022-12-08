bef91d72e6682a699a69e8557e9f34ca0ead56ca
Mert Bulutoglu <mertbulutoglu95@gmail.com>
2022-12-08 18:37:45 +0300

    Client - Building the UI for our shop -Episode9

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component,OnInit } from '@angular/core';
-import { IProduct } from './models/product';
-import { IPagination } from './models/pagination';
+
 
 @Component({
   selector: 'app-root',
@@ -10,16 +8,12 @@ import { IPagination } from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'Commerce';
-  products: IProduct[]; //Property
 
-  constructor(private http: HttpClient) {}
+
+  constructor() {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:7108/api/products?pageSize=58').subscribe((response: IPagination) => {
-      this.products = response.data;
-    }, error => {
-      console.error();
-    });
+  
 
   }
 }
