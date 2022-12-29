9d955d065e33d3f2fcf9f8d8cc7128fdb79e01c7
Mert Bulutoglu <mertbulutoglu95@gmail.com>
2022-12-29 16:29:19 +0300

    Redis&Logger&Docker Client -Basket Episode-14

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
-import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -13,6 +11,8 @@ const routes: Routes = [
   {path: 'server-error', component: ServerErrorComponent},
   {path: 'not-found', component: NotFoundComponent},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
+  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule)},
+  {path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule)},
   {path: '**', redirectTo: '', pathMatch:'full'}
 ];
 
