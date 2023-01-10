bb4c0196fc0ad789ff5974b05801c1603aba641d
Mert Bulutoglu <mertbulutoglu95@gmail.com>
2023-01-10 15:35:50 +0300

    Client -Identity -Episode17

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -106,7 +106,7 @@ namespace API.Controllers
             if(CheckEmailExistsAsync(registerDto.Email).Result.Value)
             {
                 return new BadRequestObjectResult(new ApiValidationErrorResponse{Errors = new []
-                {"Email address is in user"}});
+                {"Email address is in use"}});
             }
             var user = new AppUser
             {
