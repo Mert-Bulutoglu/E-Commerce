1752f4957175ffcbe8fff0cc9a6b4199f7700fce
Mert Bulutoglu <mertbulutoglu95@gmail.com>
2023-03-26 22:57:22 +0300

    add update password

--- Client/src/app/account/update-password/update-password.component.ts
+++ Client/src/app/account/update-password/update-password.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-update-password',
@@ -6,10 +10,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-password.component.scss']
 })
 export class UpdatePasswordComponent implements OnInit {
+  returnUrl: string;
+  loginForm: FormGroup;
+  
+  constructor(spinner: NgxSpinnerService, private accountService: AccountService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+    ) { }
 
-  constructor() { }
+  state: boolean;
 
   ngOnInit(): void {
+    this.returnUrl = this.activatedRoute.snapshot.queryParams.returnUrl || '/account/login';
+    this.createPasswordResetForm();
+    
+    this.activatedRoute.params.subscribe({
+      next: params => {
+        const userId: string = params["userId"];
+        const resetToken: string = params["resetToken"];
+        this.accountService.verifyResetToken(resetToken, userId).subscribe(() => {
+          this.state = true;
+        }, error => {
+          console.log(error)
+        });
+      }
+    });
+  }
+
+
+  onSubmit(){
+    const password = this.loginForm.get('password').value;
+    const passwordConfirm = this.loginForm.get('passwordConfirm').value;
+    let userId: string;
+    let resetToken: string;
+    this.activatedRoute.params.subscribe({
+      next: params => {
+        userId = params['userId'];
+        resetToken = params['resetToken'];
+      }
+    });
+    this.accountService.updatePassword(userId, resetToken, password, passwordConfirm).subscribe(() => {
+      this.router.navigateByUrl(this.returnUrl);
+    }, error => {
+      console.log(error);
+    });
+
+  }
+
+  createPasswordResetForm() {
+    this.loginForm = new FormGroup({
+         email: new FormControl('', [Validators.required, Validators
+        .pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]),
+        password: new FormControl('', Validators.required),
+        passwordConfirm: new FormControl('', Validators.required)
+    });
+  }
+
+  updatePassword() {
+    
   }
 
 }
